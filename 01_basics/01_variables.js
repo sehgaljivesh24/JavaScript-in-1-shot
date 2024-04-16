dfb74ca8e017df636015e90708b5e94881e48e0c
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-04-16 13:06:09 +0000

    Variables in JavaScript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+// 1. Variables and constants
+// Constant - Jisko ek baar declare krdiya usko change na kr pauin
+// Declaring constant
+const accountId = 1233445
+
+// Variables declare krne ke 2 tareeke hain - let & var(not used)
+let accountEmail = "jivesh@google.com";
+var accountPassword = "12345";
+accountCity = "Patiala";
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword]);
