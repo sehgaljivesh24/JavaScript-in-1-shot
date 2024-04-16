0e51714bb9a3e11ecd48eed997105ce7c5110fbe
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-04-16 14:04:15 +0000

    Datatypes in JS

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,1 +1,24 @@
 "use strict"; // treat all JS code as newer version
+
+// alert(3+3); // We are using Nodejs, not browser
+
+let name = "Jivesh"; // String
+let age = 23; // Number
+let isLoggedIn = false // Boolean
+let state;
+
+// 1. Primitive Datatypes 
+// number - 2^53 (Range)
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value (null matlab empty)
+// undefined => Value is not defined.
+// symbol => unique
+
+
+// 2. Reference Datatypes
+// object
+
+
+console.log(typeof age); // typeof is used to identify the 'type' of value stored in the variable 
