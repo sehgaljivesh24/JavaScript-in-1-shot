4d146ff45cfe02537394a2d5fb0cc9fa95ad0f1f
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-04-17 13:54:47 +0000

    Comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+// Comparing different Datatypes
+
+// ***NOTE - Never compare values of different datatype -- BCOZ of O/P is not Predictable 
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
