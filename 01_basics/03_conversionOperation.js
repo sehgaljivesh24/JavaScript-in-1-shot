8f963b0b2edb5395ce92c5ca308806f518394936
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-04-17 12:41:04 +0000

    Converting Values in Number, Boolean, String

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,46 @@
+// Converting values in NUMBER
+
 let score = "33abc"; // This is not a number, hence when it is converted - it gives NaN
-console.log(typeof score);
+// console.log(typeof score);
 
 // Converts string value to number datatype  -- pass the variable in Number() method
 let valueInNumber = Number(score);
 
-console.log(valueInNumber); // NaN
-console.log(typeof valueInNumber); 
+// console.log(valueInNumber); // NaN
+// console.log(typeof valueInNumber); 
 
 // NaN is also a type & we can check it if any value has given it or not.
+
+
+// *** NOTES
+// What happens when we try to convert values to Number
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => 0
+// true => 1; false => 0
+
+
+
+// Converting values in BOOLEAN
+
+let loggedIn = undefined; 
+
+let valueInBoolean = Boolean(loggedIn);
+
+console.log(valueInBoolean);
+console.log(typeof valueInBoolean);
+
+// ***NOTES
+// Javascript automatically converts values to Boolean
+// Falsy Values => 0, "" (Empty String), false, null, undefined - will be converted to - false
+// Truthy Values => All values except these will be converted to - TRUE
+
+
+
+
+let someValue = false;
+
+let valueInString = String(someValue);
+console.log(valueInString);
+console.log(typeof valueInString);
