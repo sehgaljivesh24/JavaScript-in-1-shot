a05fce41a488328f9a31366048ea4099d5dc1f76
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-04-17 14:15:49 +0000

    Datatypes Summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,8 @@
+// DataTypes are of 2 types - Primitive & Reference (Non - Primitive)
+// These are based on how the data is stored in memory and how we can access it.
+
+// Primitive Datatypes
+// Has 7 categories - String, Number, Boolean, null, undefined, bigInt, Symbol
+// Jab bhi Primitive Datatype ki value pass, copy ya assign ho rhi hoti hai - ek jagah se dusri jagah, 
+// toh Inka reference nhi diya jata - bas value ki copy di jati hai
+// Jo bhi changes krte hain, voh copy mein hoti hain
