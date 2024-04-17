08056395d3c83111e3afe69c4cae1f9bcff51500
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-04-17 13:44:14 +0000

    Converting Values & Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -44,3 +44,28 @@ let someValue = false;
 let valueInString = String(someValue);
 console.log(valueInString);
 console.log(typeof valueInString);
+
+
+
+// ******************** OPERATIONS *******************
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Hello ";
+let str2 = "Jivesh";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
+console.log(1 + "2");
+console.log("1" - 2);
