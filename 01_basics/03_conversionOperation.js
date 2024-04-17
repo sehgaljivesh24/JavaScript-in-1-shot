4755e01ed880afb259d24018db4a2ddab9e23249
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-04-17 12:19:17 +0000

    NaN

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -6,3 +6,5 @@ let valueInNumber = Number(score);
 
 console.log(valueInNumber); // NaN
 console.log(typeof valueInNumber); 
+
+// NaN is also a type & we can check it if any value has given it or not.
