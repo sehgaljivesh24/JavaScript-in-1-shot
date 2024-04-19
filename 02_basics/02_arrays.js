4baa81c8a422a115ea8a8817ff8df348ce979c45
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-04-19 16:39:32 +0000

    More Array Methods

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -19,5 +19,21 @@ console.log(`allNewHeroes: `,allNewHeroes);
 // Flatten Array
 const anotherArray = [1,2,3,[4,5,6],4,7,[6,7,[4,5]]];
 console.log(`anotherArray: `,anotherArray);
-const realAnotherArray = anotherArray.flat(1);
+
+const realAnotherArray = anotherArray.flat(1); // Returns a new Array
 console.log(`realAnotherArray: `,realAnotherArray);
+
+
+
+// Array.isArray() & Array.from()
+console.log(Array.isArray('Hitesh'));
+console.log(Array.from('Jivesh'));  // Converting String into Array
+
+
+// Creating array out of individual values
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+const newArray2 = Array.of(score1,score2,score3);
+console.log(newArray2);
