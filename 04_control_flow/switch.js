baf68dc995e7c87f29a9cfb1f59654de637bbbb7
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-05-01 13:20:52 +0000

    Switch Case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,39 @@
+// switch (key) { // Key humnein har baar check krni hai
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+const month = 3;
+
+switch (month) {
+  case 1: // Kya 1 - month mein stored value ke barabar hai ki nhi 
+    break;
+  case 2:
+    break;
+  case 3:
+    break;
+  case 4:
+    break;
+  case 5:
+    break;
+  case 6:
+    break;
+  case 7:
+    break;
+  case 8:
+    break;
+  case 9:
+    break;
+  case 10:
+    break;
+  case 11:
+    break;
+  case 12:
+    break;
+
+  default:
+    break;
+}
