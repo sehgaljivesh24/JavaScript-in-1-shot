a853fd8ac9e7029c781b696f1285111d48594d2b
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-05-01 13:05:44 +0000

    Control Flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,14 @@
+// Jab Website mein loggedIn ho - toh alag code run hona chaiye
+// Jab logged out hain - toh alag code run  hona chaiye
+
+// if
+// if (condition) {
+//     // condition must be true - if we need to execute code inside if
+// }
+
+const isUserLoggedIn = true;
+if (isUserLoggedIn) {
+  console.log(`The User is logged in`);
+}
+
+// <, >, <=, >=, !=, ==, ===
