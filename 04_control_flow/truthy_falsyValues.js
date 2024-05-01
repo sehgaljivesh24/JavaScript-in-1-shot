f264cf582beebf6577116fbb288d9b10868ec82a
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-05-01 15:01:55 +0000

    Truthy Falsy Values, Check if Array/Object is Empty, Nullish Coalescing Operator (??)

--- 04_control_flow/truthy_falsyValues.js
+++ 04_control_flow/truthy_falsyValues.js
@@ -6,12 +6,33 @@ if (userEmail) {
   console.log("Dont have user email");
 }
 
-
-// Falsy Values - which convert to false 
+// Falsy Values - which convert to false
 // false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
 // Truthy Values
-// Falsy Values ke ilava jo bhi hain voh - true mein convert hoti hain 
+// Falsy Values ke ilava jo bhi hain voh - true mein convert hoti hain
 
 // "0", "false", " " - String ke andar kuch bhi ho - Truthy value hi hogi
 // [], {}, function(){} - Truthy Value
+
+let array = [];
+if (array.length === 0) {
+  console.log("Array is empty");
+}
+
+let emptyObject = {};
+if (Object.keys(emptyObject).length === 0) {
+  console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator (??) : null undefined
+// Real situation mein hota kya hai ki - Kahin baar hum Database ko call lagate hain but koi reponse na ane pr
+// hum - null hi store krva dete hain
+
+let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+// val1 = undefined ?? 10;
+val1 = null ?? 10 ?? 20;
+
+console.log(val1);
