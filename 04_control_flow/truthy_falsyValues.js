b6971beaeacd31e8ca1897bda9728f890642d468
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-05-01 14:39:37 +0000

    truthy_falsy Values

---
+++ 04_control_flow/truthy_falsyValues.js
@@ -0,0 +1,17 @@
+const userEmail = "h@hitesh.ai";
+
+if (userEmail) {
+  console.log("Got User Email");
+} else {
+  console.log("Dont have user email");
+}
+
+
+// Falsy Values - which convert to false 
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// Truthy Values
+// Falsy Values ke ilava jo bhi hain voh - true mein convert hoti hain 
+
+// "0", "false", " " - String ke andar kuch bhi ho - Truthy value hi hogi
+// [], {}, function(){} - Truthy Value
