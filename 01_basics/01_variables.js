f6197274eb85b222c9701ec4ea37acefed6a7418
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-11-16 12:41:53 +0000

    variables in js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,9 +1,12 @@
 // 1. Variables and constants
 // Constant - Jisko ek baar declare krdiya usko change na kr pauin
+// can't be re-assigned a value and can't be declared again  (block scope)
 // Declaring constant
 const accountId = 1233445
 
 // Variables declare krne ke 2 tareeke hain - let & var(not used)
+// let = can be re-assigned a value but can't be declared again (block scope)
+// var = it can be re-assigned a value and it can also be declared again  (Global scope)
 let accountEmail = "jivesh@google.com";
 var accountPassword = "12345";
 accountCity = "Patiala";
