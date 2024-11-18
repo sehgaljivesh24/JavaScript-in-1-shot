a62f75df1b46e07c8b297cdcd2044778bf115a1e
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-11-18 10:48:28 +0000

    for of loop

---
+++ 04_control_flow/three.js
@@ -0,0 +1,8 @@
+// for of loop
+
+// Most of the time, we will get multiple objects in array
+// And we need to perform operation on them at each iteration
+
+// let array1 = [{}, {}, {}];
+const arr = [1, 2, 3, 4, 5];
+
