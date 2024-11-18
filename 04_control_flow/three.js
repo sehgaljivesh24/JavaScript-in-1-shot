8366f899d8a375dbcf6480d6493e8db0e19672f5
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-11-18 10:58:45 +0000

    for of loop

--- 04_control_flow/three.js
+++ 04_control_flow/three.js
@@ -6,3 +6,12 @@
 // let array1 = [{}, {}, {}];
 const arr = [1, 2, 3, 4, 5];
 
+// Object and Arrays both are Iterable
+// for of loop can be applied on both of them
+// Syntax -> for(let element of object){}
+// On which object we want to apply the loop on
+// It will automatically iterate over Array  and gives us element
+
+for (let element of arr) {
+    console.log(element);
+}
