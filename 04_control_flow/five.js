10bf98131955e59a9ae2218eacbb8677424b4900
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-11-18 15:07:11 +0000

    forEach method of array

--- 04_control_flow/five.js
+++ 04_control_flow/five.js
@@ -1,9 +1,23 @@
 // forEach Method
 // Takes Callback Fn in Argument
 // forEach Method calls the Callback Fn one time for each element in the Array
+// callback fn takes element as the paramter
 
 const coding = ['js', 'ruby', 'java', 'python', 'cpp'];
 
-coding.forEach(function (element) {
-    console.log(element);
+// coding.forEach(function (element) {
+//     console.log(element);
+// });
+
+// coding.forEach((element) => {
+//     console.log(element);
+// });
+
+// function printMe(item) {
+//     console.log(item);
+// }
+// coding.forEach(printMe);
+
+coding.forEach((element, index, array) => {
+    console.log(element, index, array);
 });
