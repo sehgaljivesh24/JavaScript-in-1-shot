1aebb33e5a0359688015cc5be70442a1055e2ae8
Jivesh Sehgal <101943866+sehgaljivesh24@users.noreply.github.com>
2024-11-18 12:13:29 +0000

    for-in Loop -> to loop on Objects

--- 04_control_flow/four.js
+++ 04_control_flow/four.js
@@ -1,5 +1,6 @@
 // for-in Loop on Objects
 // We can iterate on Objects using for-in Loop
+// In case of Objects => it returns keys/properties
 const myObject = {
     js: 'JavaScript',
     cpp: 'C++',
@@ -8,5 +9,16 @@ const myObject = {
 };
 
 for (const key in myObject) {
-    console.log(myObject[key]);
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+// ---------------------------------------
+console.log('---------------------------------------');
+// ---------------------------------------
+
+// for-in loop on Arrays
+// In case of arrays -> It returns Indexes
+const programming = ['js', 'ruby', 'python', 'cpp'];
+for (const key in programming) {
+    console.log(key);
 }
